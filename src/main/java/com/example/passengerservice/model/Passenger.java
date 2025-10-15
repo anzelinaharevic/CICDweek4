b7f4e8a616df9c537701186d0b66f8dfd4cd4b8c
ANZELINAHAREVICA-STU <g00414991@atu.ie>
2025-10-15 14:43:17 +0100

    Created Passenger Model

--- src/main/java/com/example/passengerservice/model/Passenger.java
+++ src/main/java/com/example/passengerservice/model/Passenger.java
@@ -1,4 +1,23 @@
 package com.example.passengerservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Passenger {
+    @NotBlank @Size(max = 40)
+    private String passengerId;
+
+    @NotBlank @Size(max = 60)
+    private String name;
+
+    @NotBlank @Email
+    private String email;
 }
+
