50996f1560df8178c8a6e8a0f4832382eb5e5b09
anz <g00414991@atu.ie>
2025-10-22 12:48:30 +0100

    Added Test to PassengerService

--- src/test/java/com/example/passengerservice/service/PassengerServiceTest.java
+++ src/test/java/com/example/passengerservice/service/PassengerServiceTest.java
@@ -1,4 +1,35 @@
 package com.example.passengerservice.service;
 
+import com.example.passengerservice.model.Passenger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PassengerServiceTest {
+
+    private PassengerService service;
+
+    @BeforeEach
+    void setUp() {service = new PassengerService();}
+
+    @Test
+    void createThenFindById() {
+        Passenger p = Passenger.builder()
+                .passengerId("P1")
+                .name("Anz")
+                .email("paul@atu.ie")
+                .build();
+
+        service.create(p);
+
+        Optional<Passenger> found = service.findById("P1");
+        assertTrue(found.isPresent());
+        assertEquals("Anz", found.get().getName());
+    }
+
+
 }
